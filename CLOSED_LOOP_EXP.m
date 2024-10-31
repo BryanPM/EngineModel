4d1a13cea282e6bf7683bd4ffae720ec5f0f2020
BryanPM <107432924+BryanPM@users.noreply.github.com>
2024-10-31 11:06:58 -0400

    Create CLOSED_LOOP_EXP.m

---
+++ CLOSED_LOOP_EXP.m
@@ -0,0 +1,47 @@
+load('/Users/1pq/Library/CloudStorage/OneDrive-OakRidgeNationalLaboratory/Research/NTRC/UTORII_2024/UTORII Data/20241028/20241028_NH3_DIQty8_SOI45_Network5_Switch_007.mat')
+
+signal = Cylinder_1_Cycle_Data.IMEPg;
+
+% Define moving window size
+window_size = 200; % Window size for moving average
+
+% Compute moving mean and moving standard deviation
+moving_mean = movmean(signal, [window_size, 0], "Endpoints", "fill");
+moving_std = movstd(signal, [window_size, 0]);
+
+% Compute coefficient of variation (CV)
+cv_values = moving_std ./ moving_mean * 100;
+
+idx = find(sgolayfilt(Cylinder_1_Cycle_Data.Injection_1_Duration, 3, 21) > 0.42, 1);
+
+figure(1)
+
+subplot(6,1,1)
+plot(-Cylinder_1_Cycle_Data.Injection_1_SOI)
+ylabel("Timing (deg)")
+xlim([0 3000])
+
+subplot(6,1,2)
+plot(Cylinder_1_Cycle_Data.Feedforward_Fuel + [zeros(1,idx), Cylinder_1_Cycle_Data.EONS_Fuel_Qty(idx+1:end)])
+ylabel("Quantity (mg)")
+xlim([0 3000])
+
+subplot(6,1,3)
+plot(Cylinder_1_Cycle_Data.CA50)
+ylabel("CA50 (deg)")
+xlim([0 3000])
+
+subplot(6,1,4)
+plot(Cylinder_1_Cycle_Data.Combustion_Efficiency)
+ylabel("\eta_c (%)")
+xlim([0 3000])
+
+subplot(6,1,5)
+plot(Cylinder_1_Cycle_Data.Cost_C)
+ylabel("Cost")
+xlim([0 3000])
+
+subplot(6,1,6)
+plot(cv_values)
+ylabel("CoV IMEP")
+xlim([0 3000])
